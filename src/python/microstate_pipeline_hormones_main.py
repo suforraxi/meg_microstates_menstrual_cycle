37055fbb1cece8cf6ef654d885dfaa6736c23995
matte <suforraxi@gmail.com>
2026-02-05 12:02:15 +0100

    change the base folder to save results in a more relative way

--- src/python/microstate_pipeline_hormones_main.py
+++ src/python/microstate_pipeline_hormones_main.py
@@ -27,7 +27,8 @@ if __name__ == "__main__":
     start_microstates = 2
     end_microstates = 41
     # output directory
-    base_output_dir = 'C:\\Users\\Admin\\Desktop\\matteo\\results\\mc_microstate\\1000'
+    base_output_dir = os.path.join(os.getcwd(), 'results', '1000')
+    
     if not os.path.exists(base_output_dir):\
         os.makedirs(base_output_dir)
     # Define sessions
