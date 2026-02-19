5d31a6eecca4bed46ed4eb42c497dec7d2fc5375
matte <suforraxi@gmail.com>
2026-02-19 09:04:37 +0100

    commenting

--- src/python/plot_brain.py
+++ src/python/plot_brain.py
@@ -13,7 +13,7 @@ if not os.path.exists(base_output_dir):
 # 1. Use a wildcard '*' to get all folders matching the pattern
 all_files = glob.glob(os.path.join(in_folder, '*_microstates_segmentation', 'microstates_all_subjects_results.npz'))
 
-# 2. Filter for the numeric range 7-21
+# 2. Filter for the numeric range 13
 filtered_files = [
     f for f in all_files 
     if 13 == int(os.path.basename(os.path.dirname(f)).split('_')[0]) 
